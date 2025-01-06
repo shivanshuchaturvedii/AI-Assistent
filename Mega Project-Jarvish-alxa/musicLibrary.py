b95bbe9bdf351f9b65c81d0a51c9edd25870828c
shivanshu chaturvedi <raghuvanshchaturvedi889@gmail.com>
2025-01-06 18:19:08 +0530

    upload code of python mega project
    add all code of python and library of song and useful thing

---
+++ Mega Project-Jarvish-alxa/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+  "stealth": "https://www.youtube.com/watch?v=U47Tr988_wE",
+  # "skyfall": 
+  "radhaRani": "https://www.youtube.com/watch?v=mjixdc3g9Cs&list=RDmjixdc3g9Cs&start_radio=1",
+
+  "o palan hare": "https://www.youtube.com/watch?v=kbMinfmC3E0"
+}
